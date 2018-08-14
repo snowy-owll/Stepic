950c3d7e904ea704a8bd410a2f507b50a13dd644
Snowy Owl <phoenix.bars@gmail.com>
2018-08-14 11:34:53 +0300

    Web-technologies 1.9.11 v1

--- init.sh
+++ init.sh
@@ -1,5 +1,4 @@
 sudo unlink /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-#sudo ﻿ln -s /home/snowy_owl/git/Stepik/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/nginx restart
-#sudo systemctl restart nginx.service
